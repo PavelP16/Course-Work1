2a384c6ab73ec90c9eca352561a3f357579d6dfe
Pavel <pavelpulle1@gmail.co>
2024-05-14 00:38:07 +0300

    Список всех сотрудников

---
+++ src/Employee.java
@@ -0,0 +1,63 @@
+import java.util.Objects;
+
+public class Employee {
+    private String name;
+    private String jobTitle;
+    private double salary;
+
+    private static int id;
+
+    public Employee(String name, String jobTitle, double salary) {
+        this.name = name;
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+        id = id + 1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Double.compare(salary, employee.salary) == 0 && Objects.equals(name, employee.name) && Objects.equals(jobTitle, employee.jobTitle);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, jobTitle, salary);
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void raiseSalary(double percentage) {
+        salary = salary + salary * percentage / 100;
+    }
+
+    public void printEmployeeDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Job Title: " + jobTitle);
+        System.out.println("Salary: " + salary);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Course Work1");
+
+        Employee employee1 = new Employee("Alexander Pushkin", "writer", 80_000);
+        Employee employee2 = new Employee("Amadeus Mozart", "Composer", 90_000);
+        Employee employee3 = new Employee("Albert Einstein", "theoretical physicist", 100_000);
+        System.out.println("\n Данные о сотрудниках");
+
+        employee1.printEmployeeDetails();
+        System.out.println("\n");
+
+        employee2.printEmployeeDetails();
+        System.out.println("\n");
+
+        employee3.printEmployeeDetails();
+        System.out.println("\n");
+
+
+
+
+
+
+
+
+
     }
 }
